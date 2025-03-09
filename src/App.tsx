084ff11b749b440a3e937f15b2200ae7203bb395
Storitelleds <douglasik95@gmail.com>
2025-03-09 08:37:13 -0300

    implement routing with Home and History pages, add DefaultLayout and Header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from "./styles/global"
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       
-      <h1>HELLO WORLD!</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      
       <GlobalStyle />
     </ThemeProvider >
 
