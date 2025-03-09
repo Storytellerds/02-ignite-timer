42a0d7b153db61601f0082a5313d5db174921d96
Storitelleds <douglasik95@gmail.com>
2025-03-08 21:05:41 -0300

    add initial pages, global styles, and theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from "./styles/global"
+
 export function App() {
 
   return (
-    <h1>HELLO WORLD!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      
+      <h1>HELLO WORLD!</h1>
+
+      <GlobalStyle />
+    </ThemeProvider >
+
   )
 }
-
